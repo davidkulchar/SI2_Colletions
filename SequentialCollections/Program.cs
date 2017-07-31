9eefedbb912d20a6b5aa73ad1ace46c211eda939
davidkulchar <davidkulchar@gmail.com>
2017-07-31 14:19:31 +0200

    Add project files.

---
+++ SequentialCollections/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SequentialCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Fourth");
+
+            while (queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+
+                Console.WriteLine("From Queue: {0}", obj);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+
+            while (stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("From Stack: {0}", obj);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Hashtable lookup = new Hashtable();
+            lookup["0"] = "Zero";
+            lookup["1"] = "One";
+            lookup["2"] = "Two";
+            lookup["3"] = "Three";
+            lookup["4"] = "Four";
+            lookup["5"] = "Five";
+            lookup["6"] = "Six";
+            lookup["7"] = "Seven";
+            lookup["8"] = "Eight";
+            lookup["9"] = "Nine";
+
+            string ourNumber = "888-555-1212";
+
+            foreach (char c in ourNumber)
+            {
+                string digit = c.ToString();
+                if (lookup.ContainsKey(digit))
+                {
+                    Console.WriteLine(lookup[digit]);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            GC g = new GC();
+            Console.ReadKey();
+        }
+    }
+}
